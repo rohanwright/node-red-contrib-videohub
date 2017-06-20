24a8dbbcbaca725dbd9b32ce7c0f00daed848c81
rohanwright <rohan.wright@kardiniachurch.com>
2017-06-20 22:26:57 +1000

    0.0.2

--- videohub/videohub.js
+++ videohub/videohub.js
@@ -1,1 +1,31 @@
-// JavaScript Document
+module.exports = function (RED) {
+	function videohub(config) {
+		RED.nodes.createNode(this, config);
+		var node = this;
+
+
+		var Router = require('io-videohub');
+		this.router = new Router({host: config.ip, port: '9991'});
+		
+		this.router.on('update', function(callback) {
+			var msg = { payload: callback };
+			node.send(msg);
+		});
+		
+	 	this.on('input', function(msg) {
+			this.router.route(msg.payload.split[0], msg.payload.split[1]);
+			
+			
+			
+			
+			
+			
+			
+		});
+		this.on('close', function () {
+
+		});
+
+	}
+	RED.nodes.registerType("videohub", videohub);
+}
